1e6377a65d08d4cf57db13a3e4b39da2cbd03346
Mihail Nikitin <nitinin.miha@icloud.com>
2021-11-25 21:13:57 +0300

    Completed homework

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Car {
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private final CountDownLatch finishCountDown;
+
+    private final Race race;
+    private int speed;
+    private String name;
+    private final CountDownLatch startCountDown;
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed, CountDownLatch startCountDown, CountDownLatch finishCountDown) {
+        this.race = race;
+        this.speed = speed;
+        this.startCountDown = startCountDown;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.finishCountDown = finishCountDown;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            startCountDown.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            synchronized (race){
+                 race.wait();
+            }
+        }
+        catch (InterruptedException exception){
+            exception.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        finishCountDown.countDown();
+    }
 }
